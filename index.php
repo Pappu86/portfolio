d0c2b644f22c16ce92725d8ee3a6eaf115139a55
Md. Pappu Miahn <cse.pappu@gmail.com>
2022-02-22 23:59:14 +0600

    Index page design

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Md.Pappu Miahn</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <!--  Pre Loader  -->
+    <div id="overlayer" style="display: none"></div>
+    <span class="loader" style="display: none"></span>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">1 of 3gdfsgdfg</div>
+            <div class="col">2 of 2dfgdfg</div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
